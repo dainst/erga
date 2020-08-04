9efee3fcad9a0e7905bb9515dd2ae3a4364c50f4
njunghans <junghans.nino@googlemail.com>
2020-08-04 15:28:15 +0200

    different json for listing

--- lib/erga_web/views/api/project_view.ex
+++ lib/erga_web/views/api/project_view.ex
@@ -2,7 +2,7 @@ defmodule ErgaWeb.Api.ProjectView do
   use ErgaWeb, :view
 
   def render("list.json", %{projects: projects}) do
-    %{data: render_many(projects, ErgaWeb.Api.ProjectView, "project.json")}
+    %{data: render_many(projects, ErgaWeb.Api.ProjectView, "project_short.json")}
   end
 
   def render("show.json", %{project: project}) do
@@ -18,6 +18,16 @@ defmodule ErgaWeb.Api.ProjectView do
       inserted_at: project.inserted_at,
       updated_at: project.updated_at,
       stakeholders: render_many(project.stakeholders, ErgaWeb.Api.StakeholderView, "stakeholder.json")
+    }
+  end
+  def render("project_short.json", %{project: project}) do
+    %{
+      id: project.id,
+      project_key: project.project_code,
+      title: project.title,
+      inserted_at: project.inserted_at,
+      updated_at: project.updated_at,
+      stakeholder_count: Enum.count(project.stakeholders)
     }
   end
 end
