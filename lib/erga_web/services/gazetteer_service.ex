8cf65b2c3d8104130960fbdd0a1722f850970e75
Nils Hempel <nils.hempel@dainst.de>
2020-08-11 15:53:04 +0200

    all forms now work with live

--- lib/erga_web/services/gazetteer_service.ex
+++ lib/erga_web/services/gazetteer_service.ex
@@ -8,12 +8,6 @@ defmodule GazetteerService do
     "https://gazetteer.dainst.org/search.json?q=" <> url <> "&sort=prefName.title.sort&offset=0&limit=10"
   end
 
-  def process_response_body(body) do
-    body
-    |> Poison.decode!
-    |> Map.take(@expected_fields)
-    |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
-  end
 
 
 end
