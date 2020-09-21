eb11be827eb5564dc50728e4d8d45afa51049ad8
njunghans <junghans.nino@googlemail.com>
2020-09-21 12:09:12 +0200

    add Research schema for links

---
+++ lib/erga/research/external_link.ex
@@ -0,0 +1,21 @@
+defmodule Erga.Research.ExternalLink do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "external_links" do
+    field :label, :string
+    field :url, :string
+
+    belongs_to :project, Erga.Research.Project
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(external_link, attrs) do
+    external_link
+    |> cast(attrs, [:label, :url])
+    |> validate_required([:label, :url])
+    |> cast_assoc(:project)
+  end
+end
