5bb23f308ee9daba5f79df43f4c23e0bb7c2d4d5
njunghans <junghans.nino@googlemail.com>
2020-11-16 15:20:10 +0100

    create first arachne project seed

---
+++ priv/repo/idai-world-seeds.exs
@@ -0,0 +1,63 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     Erga.Repo.insert!(%Erga.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+
+alias Erga.Research.ExternalLink
+alias Erga.Research.LinkedResource
+alias Erga.Research.Stakeholder
+alias Erga.Staff.Person
+alias Erga.Research.Project
+alias Erga.Accounts.User
+alias Erga.Research.TranslatedContent
+alias Erga.Research.ProjectTranslation
+
+Erga.Repo.delete_all Project
+Erga.Repo.delete_all Stakeholder
+Erga.Repo.delete_all ExternalLink
+Erga.Repo.delete_all LinkedResource
+
+
+project1 =
+  Erga.Repo.insert! %Project{
+
+    project_code: "fotoarchivdaiberlin",
+    external_links: [
+      %ExternalLink{
+        label: "Berlin head office",
+        url: "https://arachne.dainst.org/project/fotoarchivdaiberlin"
+      }
+    ]
+}
+
+Erga.Research.create_image(
+  %{
+    "label" => "1",
+    "primary" => "true",
+    "project_code" => project1.project_code,
+    "project_id" => project1.id,
+    "upload" => %{
+      filename: "idai_images_photothek_berlin.jpg",
+      url: "https://github.com/dainst/idai-world/blob/master/src/assets/images/content/what/images/idai_images_photothek_berlin.jpg?raw=true"
+    }
+  }
+)
+
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project1.description_translation_target_id,
+  language_code: "en",
+  text: "The photo archive at the Berlin head office offers images on vase painting, sculpture, lamps and topography. Among the scholarly bequests are those of Th. Wiegand, O. Reuther, F. Noack, B. Schweitzer, G. Loeschke, A. Schiff, K. Ronczewski.",
+})
+
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.title_translation_target_id,
+  language_code: "en",
+  text: "Great digging project"
+})
