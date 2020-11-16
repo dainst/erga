8f0a6fdcec0ebfadc836226ff644509bd5043ca2
njunghans <junghans.nino@googlemail.com>
2020-11-16 15:37:48 +0100

    add 3+

--- priv/repo/idai-world-seeds.exs
+++ priv/repo/idai-world-seeds.exs
@@ -94,3 +94,144 @@ Erga.Repo.insert!(%TranslatedContent{
   language_code: "en",
   text: "Berlin Orient Department"
 })
+
+#fotokairo
+project =
+  Erga.Repo.insert! %Project{
+
+    project_code: "fotokairo",
+    external_links: [
+      %ExternalLink{
+        label: "Cairo department",
+        url: "https://arachne.dainst.org/project/fotokairo"
+      }
+    ]
+  }
+Erga.Research.create_image(
+  %{
+    "label" => "1",
+    "primary" => "true",
+    "project_code" => project.project_code,
+    "project_id" => project.id,
+    "upload" => %{
+      filename: "idai_images_photothek_cairo.jpg",
+      url: "https://github.com/dainst/idai-world/blob/master/src/assets/images/content/what/images/idai_images_photothek_cairo.jpg?raw=true"
+    }
+  }
+)
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.description_translation_target_id,
+  language_code: "en",
+  text: "The photo archive of the Cairo Department focuses on the documentation of the department's excavations in Egypt. The archive comprises ca. 240,000 medium format (b/w, color) photographs, slides and 35mm-b/w-pictures."
+})
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.title_translation_target_id,
+  language_code: "en",
+  text: "Cairo Department"
+})
+
+
+#damascusbranch
+project =
+  Erga.Repo.insert! %Project{
+
+    project_code: "damascusbranch",
+    external_links: [
+      %ExternalLink{
+        label: "Damascus Branch",
+        url: "https://www.dainst.org/en/standort/-/organization-display/ZI9STUj61zKB/124822"
+      }
+    ]
+  }
+Erga.Research.create_image(
+  %{
+    "label" => "1",
+    "primary" => "true",
+    "project_code" => project.project_code,
+    "project_id" => project.id,
+    "upload" => %{
+      filename: "idai_images_photothek_damascus.jpg",
+      url: "https://github.com/dainst/idai-world/blob/master/src/assets/images/content/what/images/idai_images_photothek_damascus.jpg?raw=true"
+    }
+  }
+)
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.description_translation_target_id,
+  language_code: "en",
+  text: "The photo archive of the branch owns around 45,000 negatives and slides from DAI research projects as well as of several important ruins in Syria that were documented as a part of a photographic survey. More than 40,000 digital images are archived."
+})
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.title_translation_target_id,
+  language_code: "en",
+  text: "Damascus Branch"
+})
+
+#fotoistanbul
+project =
+  Erga.Repo.insert! %Project{
+
+    project_code: "fotoistanbul",
+    external_links: [
+      %ExternalLink{
+        label: "Istanbul department",
+        url: "https://arachne.dainst.org/project/fotoistanbul"
+      }
+    ]
+  }
+Erga.Research.create_image(
+  %{
+    "label" => "1",
+    "primary" => "true",
+    "project_code" => project.project_code,
+    "project_id" => project.id,
+    "upload" => %{
+      filename: "idai_images_photothek_istanbul.jpg",
+      url: "https://github.com/dainst/idai-world/blob/master/src/assets/images/content/what/images/idai_images_photothek_istanbul.jpg?raw=true"
+    }
+  }
+)
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.description_translation_target_id,
+  language_code: "en",
+  text: "The photo archive is made up primarily of black-and-white photographs of Turkey, its population, architecture and archaeological artifacts. Its holdings consist of approx. 120,000 photos from the period 1880 - 2007, including 65,000 negative originals."
+})
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.title_translation_target_id,
+  language_code: "en",
+  text: "Istanbul department"
+})
+
+#fotomadrid
+project =
+  Erga.Repo.insert! %Project{
+
+    project_code: "fotomadrid",
+    external_links: [
+      %ExternalLink{
+        label: "Madrid Department",
+        url: "https://arachne.dainst.org/project/fotomadrid"
+      }
+    ]
+  }
+Erga.Research.create_image(
+  %{
+    "label" => "1",
+    "primary" => "true",
+    "project_code" => project.project_code,
+    "project_id" => project.id,
+    "upload" => %{
+      filename: "idai_images_photothek_madrid.jpg",
+      url: "https://github.com/dainst/idai-world/blob/master/src/assets/images/content/what/images/idai_images_photothek_madrid.jpg?raw=true"
+    }
+  }
+)
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.description_translation_target_id,
+  language_code: "en",
+  text: "The archive holds ca. 50,500 photographs. It covers all archaeological art forms of the Iberian Peninsula from the beginning of time up to the Islamic period."
+})
+Erga.Repo.insert!(%TranslatedContent{
+  target_id: project.title_translation_target_id,
+  language_code: "en",
+  text: "Madrid department"
+})
