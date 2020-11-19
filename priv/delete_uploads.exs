ebc12bffcb65e0de9f5d8968e240a409b9d39b1d
Simon Hohl <simon.hohl@dainst.org>
2020-11-19 09:18:24 +0100

    Remove hack

--- priv/delete_uploads.exs
+++ priv/delete_uploads.exs
@@ -2,10 +2,6 @@ require Logger
 
 dir = Application.get_env(:erga, :uploads_directory)
 
-if dir do
-  # dir happends to be nil in some case wich would resoult in an error
-
-  Logger.info("Deleting upload directory './#{dir}':")
-  File.rm_rf!(dir)
-  |> Enum.each(fn item -> Logger.info(item) end)
-end
+Logger.info("Deleting upload directory './#{dir}':")
+File.rm_rf!(dir)
+|> Enum.each(fn item -> Logger.info(item) end)
