ad339081ce45a5c4cfb851e3652eab2c70784f1f
Simon Hohl <simon.hohl@dainst.org>
2020-12-01 12:10:15 +0100

    SD-938: Rework script to add users in production

--- lib/erga/release.ex
+++ lib/erga/release.ex
@@ -14,8 +14,24 @@ defmodule Erga.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  # Run ` _build/prod/rel/erga/bin/erga eval 'Erga.Release.add_user("<user email>", "<user password>")'` from the command line.
+  def add_user(email, password) do
+    start_repo()
+
+    Erga.Repo.insert!(%Erga.Accounts.User{
+      email: email,
+      password_hash: Pow.Ecto.Schema.Password.pbkdf2_hash(password)
+    })
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp start_repo do
+    load_app()
+    Application.put_env(@app, :only_repo, true)
+    Application.ensure_all_started(@app)
   end
 
   defp load_app do
