581125954f6174043adf2b0d017170662b9035d4
Simon Hohl <simon.hohl@dainst.org>
2020-12-02 10:45:50 +0100

    Use dummy project seeds as default
    The dummy project uses more features, while the idai-world data is  sparse.

--- mix.exs
+++ mix.exs
@@ -71,7 +71,14 @@ defmodule Erga.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/delete_uploads.exs", "run priv/repo/seeds.exs", "run priv/repo/idai-world-seeds.exs"],
+      "ecto.setup": [
+        "ecto.create",
+        "ecto.migrate",
+        "run priv/delete_uploads.exs",
+        "run priv/repo/seeds.exs",
+        "run priv/repo/dummy-projects-seeds.exs",
+        # "run priv/repo/idai-world-projects-seeds.exs"
+      ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
