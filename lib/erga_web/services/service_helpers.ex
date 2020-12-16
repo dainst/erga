856878a12de0aebbc154e5944362d0087e9a0bd8
Simon Hohl <simon.hohl@dainst.org>
2020-12-16 13:39:18 +0100

    Fix chronontology service name

--- lib/erga_web/services/service_helpers.ex
+++ lib/erga_web/services/service_helpers.ex
@@ -3,7 +3,7 @@ defmodule ServiceHelpers do
   def get_system_service(system_name) do
     case String.downcase(system_name) do
       "gazetteer" -> GazetteerService
-      "chrontology" -> ChrontologyService
+      "chronontology" -> ChronontologyService
       "thesaurus" -> ThesaurusService
       "arachne" -> ArachneService
       _ -> raise "no matching service"
