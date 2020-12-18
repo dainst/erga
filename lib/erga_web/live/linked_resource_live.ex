b791057e32f7b09161fc637c753150e258b65c26
Simon Hohl <simon.hohl@dainst.org>
2020-12-18 15:56:57 +0100

    Refactor linked resources event handling

--- lib/erga_web/live/linked_resource_live.ex
+++ lib/erga_web/live/linked_resource_live.ex
@@ -4,7 +4,17 @@ defmodule ErgaWeb.LinkedResourceLive do
   alias Erga.Research
   alias Erga.Research.LinkedResource
 
-  def mount(%{"project_id" => project_id}, _session, socket) do
+  defp apply_default_assigns(socket, session) do
+    socket
+    |> assign(description_input_text: "")
+    |> assign(description_input_language_code: session["locale"])
+    |> assign(:search_result, [])
+    |> assign(:search_string, "")
+    |> assign(:linked_id, nil)
+    |> assign(:search_filter, "populated-place")
+  end
+
+  def mount(%{"project_id" => project_id}, session, socket) do
     # new case
     changeset =
       Research.change_linked_resource(%LinkedResource{})
@@ -13,33 +23,32 @@ defmodule ErgaWeb.LinkedResourceLive do
     socket =
       socket
       |> assign(page_title: "New Linked Resource")
+      |> apply_default_assigns(session)
       |> assign(project_id: project_id)
+      |> assign(descriptions: [])
       |> assign(changeset: changeset)
       |> assign(:linked_system, "gazetteer")
       |> assign(:label, "")
       |> assign(:uri, "")
-      |> assign(:search_result, [])
-      |> assign(:search_string, "")
-      |> assign(:search_filter, "populated-place")
+
     {:ok, socket}
   end
 
-  def mount(%{"id" => id}, _url, socket) do
+  def mount(%{"id" => id}, session, socket) do
     # edit case
     linked_resource = Research.get_linked_resource!(id)
     changeset = Research.change_linked_resource(linked_resource)
     socket =
       socket
       |> assign(page_title: "Edit Linked Resource")
-      |> assign(project_id: linked_resource.project_id)
+      |> apply_default_assigns(session)
       |> assign(linked_resource: linked_resource)
+      |> assign(project_id: linked_resource.project_id)
+      |> assign(descriptions: linked_resource.descriptions)
       |> assign(changeset: changeset)
       |> assign(:linked_system, linked_resource.linked_system)
       |> assign(:label, linked_resource.label)
       |> assign(:uri, linked_resource.uri)
-      |> assign(:search_string, "")
-      |> assign(:search_result, [])
-      |> assign(:search_filter, "populated-place")
 
     {:ok, socket}
   end
@@ -49,38 +58,56 @@ defmodule ErgaWeb.LinkedResourceLive do
        :new -> Phoenix.View.render(ErgaWeb.LinkedResourceView, "new.html", assigns)
        :edit -> Phoenix.View.render(ErgaWeb.LinkedResourceView, "edit.html", assigns)
     end
-
   end
 
-  @spec handle_event(<<_::32, _::_*8>>, map, Phoenix.LiveView.Socket.t()) :: {:noreply, any}
-  def handle_event("form_change", params, socket) do
-    {:noreply, EventHandler.form_change(params, socket)}
+  def handle_event("form_change", %{"_target" => ["linked_resource", target]} = params, socket) do
+    {:noreply, EventHandler.form_change(target, params, socket)}
+  end
+
+  def handle_event("form_change", _params, socket) do
+    {:noreply, socket}
   end
 
   def handle_event("choose_resource", params, socket) do
     {:noreply, EventHandler.choose_resource(params, socket)}
   end
 
-  def handle_event("save", %{"linked_resource" => linked_resource_params}, socket) do
-    case socket do
-      %{assigns: %{live_action: :edit}} ->
-          Research.update_linked_resource(socket.assigns.linked_resource, linked_resource_params)
-          |> process_action_result("Linked resource updated successfully.", socket)
-      %{assigns: %{live_action: :new}} ->
-          Research.create_linked_resource(linked_resource_params)
-          |> process_action_result("Linked resource created successfully.", socket)
+  def handle_event("add_description", params, socket) do
+    {:noreply, EventHandler.add_description(params, socket)}
+  end
+
+  def handle_event(
+    "save",
+    %{"linked_resource" => linked_resource_params},
+    %{assigns: %{live_action: :new}} = socket
+    ) do
+    case Research.create_linked_resource(linked_resource_params) do
+      {:ok, linked_resource} ->
+        {
+          :noreply,
+          socket
+          |> put_flash(:info, "Linked resource created successfully.")
+          |> redirect(to: Routes.project_path(ErgaWeb.Endpoint, :edit, linked_resource.project_id))
+        }
+      {:error, changeset } ->
+        {:noreply, assign(socket, changeset: changeset)}
     end
   end
 
-  defp process_action_result(result, flash_msg, socket) do
-    case result do
+  def handle_event(
+    "save",
+    %{"linked_resource" => linked_resource_params},
+    %{assigns: %{live_action: :edit}} = socket
+    ) do
+    case Research.update_linked_resource(socket.assigns.linked_resource, linked_resource_params) do
       {:ok, linked_resource} ->
-        {:noreply,
-        socket
-        |> put_flash(:info, flash_msg)
-        |> redirect(to: Routes.project_path(ErgaWeb.Endpoint, :edit, linked_resource.project_id))}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
+        {
+          :noreply,
+          socket
+          |> put_flash(:info, "Linked resource updated successfully.")
+          |> redirect(to: Routes.project_path(ErgaWeb.Endpoint, :edit, linked_resource.project_id))
+        }
+      {:error, changeset } ->
         {:noreply, assign(socket, changeset: changeset)}
     end
   end
--- lib/erga_web/live/linked_resource_live/event_handler.ex
+++ lib/erga_web/live/linked_resource_live/event_handler.ex
@@ -1,37 +1,40 @@
 defmodule EventHandler do
   import Phoenix.LiveView, only: [assign: 3, update: 3]
-  alias Erga.Research.LinkedResource
+  alias Erga.Research.TranslatedContent
 
 
-  def form_change(%{"_target" => targets,  "linked_resource" => params}, socket) do
-    cond do
-      "linked_system" in targets ->
-        assign(socket, :uri, "")
-        |> assign(:search_result, [])
-        |> assign(:linked_system, params["linked_system"])
-      "search_filter" in targets -> assign(socket, :search_filter, params["search_filter"])
-      "search_string" in targets -> search(socket, params["search_string"])
-      true -> validate(params, socket)
+
+  def form_change(target, %{"linked_resource" => params}, socket) do
+    case target do
+      "linked_system" ->
+        update(socket, :uri, fn _ -> "" end)
+        |> update(:search_result, fn _ -> [] end)
+        |> update(:linked_system, fn _ -> params["linked_system"] end)
+      "search_string" ->
+        search(socket, params["search_string"])
+      target ->
+        update(socket, String.to_existing_atom(target), fn _ -> params[target] end)
     end
   end
 
   def choose_resource( %{"id" => id, "name" => name, "uri" => uri}, socket) do
     socket
-      |> assign(:label, name)
-      |> assign(:linked_id, id)
-      |> assign(:uri, uri)
-      |> assign(:search_string, "")
-      |> assign(:search_result, [])
+      |> update(:label, fn _ -> name end)
+      |> update(:linked_id, fn _ -> id end)
+      |> update(:uri, fn _ -> uri end)
+      |> update(:search_string, fn _ -> "" end)
+      |> update(:search_result, fn _ -> [] end)
   end
 
-  defp validate(params, socket) do
-    changeset =
-      %LinkedResource{}
-      |> Erga.Research.change_linked_resource(params)
-      |> Map.put(:action, :insert)
+  def add_description(%{"language-code" => language_code, "text" => text}, socket) do
 
-      assign(socket, :changeset, changeset)
-      |> assign(:linked_system, params["linked_system"])
+    new_description = %TranslatedContent{
+      language_code: language_code,
+      text: text
+    }
+
+    socket
+    |> update(:descriptions, fn existing_descriptions -> existing_descriptions ++ [new_description] end)
   end
 
   defp search(socket, search_string) do
