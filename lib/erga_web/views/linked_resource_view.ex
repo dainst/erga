d8ff4b51ba1c1c8e56544b647429f92d315011c6
Simon Hohl <simon.hohl@dainst.org>
2021-01-06 10:59:33 +0100

    Remove deprecated doc string

--- lib/erga_web/views/linked_resource_view.ex
+++ lib/erga_web/views/linked_resource_view.ex
@@ -1,8 +1,3 @@
 defmodule ErgaWeb.LinkedResourceView do
   use ErgaWeb, :view
-
-  @doc """
-  Get description text and language code based on changeset changes (used for newly added translations)
-  or based on changeset data (used for loaded translations).
-  """
 end
