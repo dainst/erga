b1036ba69e15414cba8a1ea3b5660b0391262595
Nils Hempel <nils.hempel@dainst.de>
2021-01-18 15:25:57 +0100

    D-1048 - now with res_id

--- lib/erga_web/views/api/linked_resource_view.ex
+++ lib/erga_web/views/api/linked_resource_view.ex
@@ -5,8 +5,18 @@ defmodule ErgaWeb.Api.LinkedResourceView do
     %{
       linked_system: lr.linked_system,
       uri: lr.uri,
+      res_id: read_id_from_uri(lr.uri ),
       labels: render_many(lr.labels, ErgaWeb.Api.TranslatedContentView, "translated_content.json"),
       descriptions: render_many(lr.descriptions, ErgaWeb.Api.TranslatedContentView, "translated_content.json")
     }
+  end
+
+  def read_id_from_uri( uri ) do
+    parser = fn
+        "https://gazetteer.dainst.org/place/" <> id  -> id
+        "https://chronontology.dainst.org/period/" <> id -> id
+        _ -> raise "No match for given uri please add pattern to function"
+    end
+    parser.(uri)
   end
 end
