5d5c762de3551b9fcc6300437b4ff48968005e90
Simon Hohl <simon.hohl@dainst.org>
2021-05-26 09:27:07 +0200

    Pattern match on stakeholder type

--- lib/erga/staff/stakeholder.ex
+++ lib/erga/staff/stakeholder.ex
@@ -3,7 +3,7 @@ defmodule Erga.Staff.Stakeholder do
   import Ecto.Changeset
 
   schema "stakeholders" do
-    field :type, Ecto.Enum, values: [:person, :institution]
+    field :type, Ecto.Enum, values: [:person, :institution], default: :person
     field :first_name, :string
     field :last_name, :string
     field :title, :string, default: ""
@@ -16,114 +16,85 @@ defmodule Erga.Staff.Stakeholder do
     timestamps()
   end
 
-  def changeset(%{"person" => type} = stakeholder, %{} = attrs) do
+  def changeset(stakeholder, %{} = attrs) do
     stakeholder
     |> cast(attrs, [
       :type, :first_name, :last_name, :title, :orc_id, :organization_name, :ror_id
     ])
-    |> person_evaluate_field_combination(attrs)
-    |> person_evaluate_external_ids(attrs)
+    |> extract_type()
+    |> evaluate_required_fields()
+    |> evaluate_external_ids()
     |> cast_assoc(:stakeholder_to_projects)
   end
-  def changeset(%{"institution" => type} = stakeholder, %{} = attrs) do
-    stakeholder
-    |> cast(attrs, [
-      :type, :first_name, :last_name, :title, :orc_id, :organization_name, :ror_id
-    ])
-    |> institution_evaluate_field_combination(attrs)
-    |> institution_evaluate_external_ids(attrs)
-    |> cast_assoc(:stakeholder_to_projects)
-  end
-    def changeset(stakeholder, %{} = attrs) do
-      stakeholder
-      |> cast(attrs, [
-        :type, :first_name, :last_name, :title, :orc_id, :organization_name, :ror_id
-      ])
-      |> institution_evaluate_field_combination(attrs)
-      |> institution_evaluate_external_ids(attrs)
-      |> cast_assoc(:stakeholder_to_projects)
+
+  defp extract_type(changeset) do
+    { get_field(changeset, :type), changeset }
   end
 
-  @doc false
-  def changeset(stakeholder, attrs) do
-    case attrs["type"] do
-      :person ->
-        stakeholder
-        |> cast(attrs, [
-          :type, :first_name, :last_name, :title, :orc_id
-        ])
-        |> person_evaluate_field_combination(attrs)
-        |> person_evaluate_external_ids(attrs)
-        |> cast_assoc(:stakeholder_to_projects)
-      :institution ->
-        stakeholder
-        |> cast(attrs, [
-          :type, :organization_name, :ror_id
-        ])
-        |> institution_evaluate_field_combination(attrs)
-        |> institution_evaluate_external_ids(attrs)
-        |> cast_assoc(:stakeholder_to_projects)
-    end
+  def evaluate_required_fields({:institution, changeset}) do
 
+    changeset =
+      cond do
+        not is_nil(get_field(changeset, :ror_id)) && is_nil(get_field(changeset, :organization_name)) ->
+          changeset
+          |> add_error(:organization_name, "Please provide organization name when setting RORID.")
+
+        [:organization_name, :ror_id]
+        |> Enum.map(&get_field(changeset, &1))
+        |> Enum.filter(fn val -> not is_nil(val) end)
+        |> Enum.empty? ->
+          changeset
+          |> add_error(:general, "Please input some data.")
+
+        true ->
+          changeset
+      end
+
+    {:institution, changeset}
   end
 
-  def person_evaluate_field_combination(changeset, _attrs) do
-    cond do
-      not is_nil(get_field(changeset, :first_name)) && is_nil(get_field(changeset, :last_name)) ->
-        changeset
-        |> add_error(:last_name, "Please provide last name when setting first name.")
+  def evaluate_required_fields({type_atom, changeset}) do
+    changeset =
+      cond do
+        not is_nil(get_field(changeset, :first_name)) && is_nil(get_field(changeset, :last_name)) ->
+          changeset
+          |> add_error(:last_name, "Please provide last name when setting first name.")
 
-      not is_nil(get_field(changeset, :orc_id)) && is_nil(get_field(changeset, :last_name)) ->
-        changeset
-        |> add_error(:last_name, "Please provide last name when setting ORCID.")
+        not is_nil(get_field(changeset, :orc_id)) && is_nil(get_field(changeset, :last_name)) ->
+          changeset
+          |> add_error(:last_name, "Please provide last name when setting ORCID.")
 
 
-      [:first_name, :last_name, :orc_id]
-      |> Enum.map(&get_field(changeset, &1))
-      |> Enum.filter(fn val -> not is_nil(val) end)
-      |> Enum.empty? ->
-        changeset
-        |> add_error(:general, "Please input some data.")
+        [:first_name, :last_name, :orc_id]
+        |> Enum.map(&get_field(changeset, &1))
+        |> Enum.filter(fn val -> not is_nil(val) end)
+        |> Enum.empty? ->
+          changeset
+          |> add_error(:general, "Please input some data.")
 
-      true ->
-        changeset
-    end
+        true ->
+          changeset
+      end
+
+    {type_atom, changeset}
   end
 
-  def person_evaluate_external_ids(changeset, _attrs) do
-    cond do
-      not is_nil(get_field(changeset, :orc_id)) && not String.match?(get_field(changeset, :orc_id), ~r/^https:\/\/orcid.org\/([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})$/) ->
-        changeset
-        |> add_error(:orc_id, "Not a valid ORCID, expected pattern: 'https://orcid.org/XXXX-XXXX-XXXX-XXXX'")
-      true ->
-        changeset
-    end
-  end
-
-  def institution_evaluate_field_combination(changeset, _attrs) do
-    cond do
-      not is_nil(get_field(changeset, :ror_id)) && is_nil(get_field(changeset, :organization_name)) ->
-        changeset
-        |> add_error(:organization_name, "Please provide organization name when setting RORID.")
-
-      [:organization_name, :ror_id]
-      |> Enum.map(&get_field(changeset, &1))
-      |> Enum.filter(fn val -> not is_nil(val) end)
-      |> Enum.empty? ->
-        changeset
-        |> add_error(:general, "Please input some data.")
-
-      true ->
-        changeset
-    end
-  end
-
-  def institution_evaluate_external_ids(changeset, _attrs) do
+  def evaluate_external_ids({:institution, changeset}) do
     cond do
       not is_nil(get_field(changeset, :ror_id)) && not String.match?(get_field(changeset, :ror_id), ~r/^https:\/\/ror.org\/0[^ILO]{6}[0-9]{2}$/) ->
         changeset
         |> add_error(:ror_id, "Not a valid RORID, expected pattern: 'https://ror.org/0XXXXXXXX'")
 
+      true ->
+        changeset
+    end
+  end
+
+  def evaluate_external_ids({_type, changeset}) do
+    cond do
+      not is_nil(get_field(changeset, :orc_id)) && not String.match?(get_field(changeset, :orc_id), ~r/^https:\/\/orcid.org\/([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})$/) ->
+        changeset
+        |> add_error(:orc_id, "Not a valid ORCID, expected pattern: 'https://orcid.org/XXXX-XXXX-XXXX-XXXX'")
       true ->
         changeset
     end
